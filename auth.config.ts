155991f05914762eee5a5e8ea23ea5f1b4408af7
Enoch Ansah <enocansah@yahoo.com>
2024-11-19 11:09:33 +0000

    (broken) update auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,45 +1,76 @@
 import bcrypt from "bcryptjs";
+import NextAuthConfig from "next-auth";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
-import Facebook from "next-auth/providers/facebook";
 import { LoginSchema } from "@/lib/auth/schemas";
 import { getUserByEmail } from "@/lib/auth/data";
+import { JWT } from "next-auth/jwt";
+import { DefaultSession, Session, User } from "next-auth";
+import { UserRole } from "@prisma/client";
 
-export const authOptions = {
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string;
+      role?: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    role?: UserRole;
+  }
+}
+
+export default NextAuth({
+  pages: {
+    signIn: "/auth",
+    error: "/auth/error",
+  },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // Facebook({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    // }),
-    // Credentials({
-    //   credentials: [
-    //     {
-    //       label: 'Email',
-    //       type: 'text',
-    //     },
-    //     {
-    //       label: 'Password',
-    //       type: 'password',
-    //     },
-    //   ],
-    //   async authorize(credentials) {
-    //     const validatedFields = LoginSchema.safeParse(credentials);
-    //     if (validatedFields.success) {
-    //       const { email, password } = validatedFields.data;
-    //       const user = await getUserByEmail(email);
-    //       if (!user || !user.password) return null;
-    //       const passwordsMatch = await bcrypt.compare(password, user.password);
-    //       if (passwordsMatch) return user;
-    //     }
-    //     return null;
-    //   },
-    // }),
-  ],
-};
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
 
-export default NextAuth(authOptions);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT & { role?: UserRole };
+    }) {
+      if (session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}) satisfies typeof NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,47 @@
 import NextAuth from "next-auth";
-// import authConfig from "@/auth.config";
-import { NextRequest } from "next/server";
+import authConfig from "@/auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
 
-// const { auth } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
 
-// export default auth((req: any) => {
-//   // const isLoggedIn = !!req.auth;
-//   // const { nextUrl } = req;
-//   // const isApiAuthRoute = nextUrl.pathname.startsWith("/api/auth");
-//   // const isAuthRoute = nextUrl.pathname.startsWith("/auth");
-//   // const isCheckoutRoute = nextUrl.pathname.startsWith("/checkout");
-//   // const isAccountRoute = nextUrl.pathname.startsWith("/account");
-//   // if (isApiAuthRoute) {
-//   //   return null;
-//   // }
-//   // if (isAuthRoute) {
-//   //   if (isLoggedIn) {
-//   //     return Response.redirect(new URL("/", nextUrl));
-//   //   }
-//   //   return null;
-//   // }
-//   // if (!isLoggedIn && (isCheckoutRoute || isAccountRoute)) {
-//   //   let callbackUrl = nextUrl.pathname;
-//   //   if (nextUrl.search) {
-//   //     callbackUrl += nextUrl.search;
-//   //   }
-//   //   const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-//   //   return Response.redirect(
-//   //     new URL(`/auth?callbackUrl=${encodedCallbackUrl}`, nextUrl),
-//   //   );
-//   // }
-//   // return null;
-// });
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-export async function middleware(req: NextRequest) {
-  return null;
-}
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-// Optionally, don't invoke Middleware on some paths
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    );
+  }
+});
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
